198a29018da54928f97067a075c378770baab54a
zhangzexi <zhang-zexi@sjtu.edu.cn>
2024-10-30 10:27:17 +0800

    to run Solar

---
+++ scripts/run_1029.sh
@@ -0,0 +1,12 @@
+python -u run_longExp.py --dataset Solar  --model MTGNN3 --seq_len 336 --pred_len 24 --itr 1 --learning_rate 0.005 --backbone --gpu 0 --batch_size=16
+wait
+python -u run_longExp.py --dataset Solar  --model MTGNN3 --seq_len 336 --pred_len 48 --itr 1 --learning_rate 0.005 --backbone --gpu 0 --batch_size=16
+wait
+python -u run_longExp.py --dataset Solar  --model MTGNN3 --seq_len 336 --pred_len 96 --itr 1 --learning_rate 0.005 --backbone --gpu 0 --batch_size=16
+wait
+python -u run_longExp.py --dataset Solar  --model MTGNN3 --seq_len 336 --pred_len 192 --itr 1 --learning_rate 0.005 --backbone --gpu 0 --batch_size=16
+wait
+python -u run_longExp.py --dataset Solar  --model MTGNN3 --seq_len 336 --pred_len 336 --itr 1 --learning_rate 0.005 --backbone --gpu 0 --batch_size=16
+wait 
+python -u run_longExp.py --dataset Solar  --model MTGNN3 --seq_len 336 --pred_len 720 --itr 1 --learning_rate 0.005 --backbone --gpu 0 --batch_size=16
+wait
